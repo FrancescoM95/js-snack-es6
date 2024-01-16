d7964df2d69d9a233e784e35335b0e1d3cd82e16
FrancescoM95 <mon95fran@gmail.com>
2024-01-16 15:23:52 +0100

    snack 1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
 console.log('JS OK');
+
+//--------------------------------
+//* SNACK 1
+//--------------------------------
+
+const tableName = 'Tavolo Vip';
+
+const guests = [
+    'Brad Pitt',
+    'Johnny Depp',
+    'Lady Gaga',
+    'Cristiano Ronaldo',
+    'Georgina Rodriguez',
+    'Chiara Ferragni',
+    'Fedez',
+    'George Clooney',
+    'Amal Clooney',
+    'Maneskin'
+];
+
+const guestsList = guests.map((guest, i) => ({
+    tableName: tableName,
+    guestName: guest,
+    seatNumber: ++i
+}));
+
+console.log(guestsList);
